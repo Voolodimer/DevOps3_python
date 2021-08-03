0c6600d2fb397cfae958e2c0665e306e0b7a60ca
Voolodimer <vladimirbruho@gmail.com>
2021-08-03 13:35:58 +0300

    add solve to Problem9 in hw9

--- hw9.py
+++ hw9.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python3
+from functools import reduce
+
 # Problem 6 Разница между квадратом суммы и суммы квадратов чисел от 1 до 100
 print(sum([x for x in range(1, 101)]) ** 2 - sum([x*x for x in range(1, 11)]))
-# Problem 9
-# [print(x, ((x * x) + (x + 1) * (x + 1)), ((x + 2) * (x + 2))) for x in range(1, 10000000000100) if ((x * x) + (x + 1) * (x + 1)) == ((x + 2) * (x + 2))]
+# Problem 9 There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
+[print(a, b, c) for a in range(1, 1001) for b in range(1, 1001) for c in range(1, 1001) if a + b + c == 1000 and a ** 2 + b ** 2 == c ** 2]
 # Problem 40
 
 # Problem 48
-ans = sum(pow(i, i) for i in range(1, 1001)) % 10000000000
+print(sum(pow(i, i) for i in range(1, 1001)) % 10000000000)
+
+
+
