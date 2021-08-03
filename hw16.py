4f5c325c813a50588fff06fb5c0ea3db1251ec93
Voolodimer <vladimirbruho@gmail.com>
2021-08-03 16:18:26 +0300

    add file hw16 and coordinates.txt

---
+++ hw16.py
@@ -0,0 +1,16 @@
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="hw16")
+coord = ''
+with open('coordinates.txt') as file:
+    # считываем координаты, разбиваем по ;, меняем , на .
+    coord = [','.join(el.rstrip('\n').replace(',', '.').replace('\'', '').split(';')) for el in file.readlines()]
+
+
+for el in coord:
+    try:
+        location = geolocator.reverse(el)
+        print('Location:', location.address)
+        print('Goggle Maps URL:', f'https://www.google.com/maps/search/?api=1&query={el}')
+    except:
+        print('error')
