f9107ae6243bedd91272727556c0013b61ea1611
Voolodimer <vladimirbruho@gmail.com>
2021-08-16 18:02:56 +0300

    added check_response function in hw19.py, renew hw19-client.py

--- hw19-client.py
+++ hw19-client.py
@@ -5,7 +5,8 @@ import socket as sock
 client_socket = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
 
 # my_req = "google.com"
-client_socket.sendto(bytes(sys.argv[1], 'utf-8'), ("127.0.0.1", 1053))
+data = " ".join(sys.argv[1:])
+client_socket.sendto(bytes(data, 'utf-8'), ("127.0.0.1", 1053))
 
 data, ipaddress = client_socket.recvfrom(1000)
 client_socket.close()
--- hw19.py
+++ hw19.py
@@ -8,8 +8,11 @@
 
 import socket as sock
 
+user_domains = {}
+
 
 def check_str_type(host):
+    # Проверяем состоит ли запрос из ip-адреса или из текста
     i = len(host.decode()) - 1
     host_str = host.decode()
     is_str = False
@@ -22,10 +25,33 @@ def check_str_type(host):
     return is_str
 
 
+def check_response(user_request):
+    # тип запроса: выдать IP или добавить запись
+    # Пример запроса: ADD my.google.com:228.228.228.228
+    # Если добавили запись return 2, если не удалось добавить return 0
+    # Если есть запись в user_domains return 1
+    if user_request.decode().lower().startswith('add '):
+        try:
+            domain, address = user_request.decode().split()[1].split(':')
+            user_domains[domain] = address
+            # print(user_domains)
+            return 2
+        except:
+            return 0
+    else:
+        if user_request.decode() in user_domains:
+            return 1
+
+
 def check_host(host):
     try:
         if check_str_type(host):
-            return sock.gethostbyname(host.decode()).encode()
+            if check_response(host) == 2:
+                return 2
+            elif check_response(host):
+                return user_domains[host.decode().lstrip()].encode()
+            else:
+                return sock.gethostbyname(host.decode()).encode()
     except sock.gaierror:
         return 0
 
@@ -37,11 +63,9 @@ with sock.socket(sock.AF_INET, sock.SOCK_DGRAM) as dns_sock:
         print(f"Подключение с {address} установлено")
         print(f"Запрос клиента: {data.decode()}")
         result = check_host(data)
-        if result:
+        if result == 2:
+            dns_sock.sendto(b"Data added successfully " + data, address)
+        elif result:
             dns_sock.sendto(result, address)
         else:
             dns_sock.sendto(b"Unable to locate " + data, address)
-
-
-
-
